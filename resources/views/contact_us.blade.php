206000360cb2e69e19385601335d6a38d64bde4a
Frances-Riel <130945535+Frances-Riel@users.noreply.github.com>
2023-06-05 10:02:29 +0800

    pages

---
+++ resources/views/contact_us.blade.php
@@ -0,0 +1,4 @@
+    @@extends('template.app')
+    @@section('content')
+        <h1>Animation</h1>
+    @endsections
---
+++ resources/views/profile/blade.php
@@ -0,0 +1,5 @@
+    @@extends('template.app')
+    @@section('content')
+        <h1>Contact_Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas maiores consequuntur magni neque maxime molestiae temporibus reiciendis deleniti? Assumenda sapiente deserunt delectus beatae aperiam dolor dolores cumque, consequatur enim quam.</p>
+    @endsections
